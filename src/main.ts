c36bd82fdfed23d4103485486f6942a444e5a11b
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-28 14:48:16 -0700

    feat: setup main function

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import './style/style.css';
+import World from './World/World';
+
+/** Todo:
+ * Create Debug UI
+ * make debug UI optional via query parameters
+ */
 
 const main = () => {
-  const app = document.createElement('div');
-  app.id = 'app';
-  document.body.appendChild(app);
+  // get reference to #app div element
+  const app = document.querySelector('#app');
+  // Create #canvasHolder div element
+  const canvasHolder = document.createElement('div');
+  canvasHolder.id = 'canvasHolder';
+  // nest canvasHolder inside of #app div
+  app?.appendChild(canvasHolder);
+
+  // create World
+  const world = new World(canvasHolder);
+
+  // render loop
+  const render = (time: number) => {
+    const deltaTime = time * 0.001;
+    world.render();
+    // loop
+    requestAnimationFrame(render);
+  };
+
+  requestAnimationFrame(render);
 };
+
 window.addEventListener('load', main);
