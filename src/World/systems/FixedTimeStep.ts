a850a3348aab518427d9356e944583bf9f43b58b
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-29 13:20:09 -0700

    feat: create 'FixedTimeStep' class

---
+++ src/World/systems/FixedTimeStep.ts
@@ -0,0 +1,31 @@
+export default class FixedTimeStep {
+  private _timeStep: number;
+  private _stepsPerSecond: number;
+  private _remainder: number;
+  private _updateFn: (timeStep: number) => void; // update function
+
+  constructor(stepsPerSecond: number, updateFn: (timeStep: number) => void) {
+    this._stepsPerSecond = stepsPerSecond;
+    this._timeStep = 60 / stepsPerSecond;
+    this._remainder = 0;
+    this._updateFn = updateFn;
+  }
+
+  public get timeStep(): number {
+    return this._timeStep;
+  }
+  public get stepsPerSecond(): number {
+    return this.stepsPerSecond;
+  }
+
+  update(deltaTime: number) {
+    // determine how many updates we need to do for this frame
+    const numOfStepsFloat = this._stepsPerSecond * deltaTime + this._remainder;
+    // this value will likely be a floating point number, so
+    // we must truncate it to an integer
+    const numOfStepsInt = Math.floor(numOfStepsFloat);
+
+    // Save the truncated part to add it on during the next update
+    this._remainder = numOfStepsFloat - numOfStepsInt;
+  }
+}
