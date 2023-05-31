d7702cf6c9335962fff8fd1ecaa0d7d38a46613f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 19:39:09 -0700

    feat: create 'App' component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div id="App">
+      <div id="canvasHolder">
+        <canvas id="canvas" />
+      </div>
+    </div>
+  );
+}
