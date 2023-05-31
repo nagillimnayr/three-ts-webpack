16e6f4133a8962d455a7626c814668d2183cf885
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-31 14:44:52 -0700

    docs: add description comment to 'DataManager' class

--- src/World/systems/DataManager.ts
+++ src/World/systems/DataManager.ts
@@ -1,10 +1,14 @@
-/**
- * class for recording and managing data
- * collected during the simulation.
- */
-
 import Body from '../components/Body';
 
+/**
+ * @description Manages and records data collected during the running of
+ * the simulation.
+ *
+ * @author Ryan Milligan
+ * @date 31/05/2023
+ * @export
+ * @class DataManager
+ */
 export class DataManager {
   trackedBody: Body; // Body to track and record data from
 }
