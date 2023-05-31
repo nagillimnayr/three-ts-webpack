d4e5ab19060c5f1022671cdc1c6bac59cb352df7
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-31 09:47:05 -0700

    refactor: rename guiPanels' to bodyController'

--- src/World/gui/bodyController.ts
+++ src/World/gui/bodyController.ts
@@ -1,7 +1,7 @@
 import GUI from 'lil-gui';
 import Body from '../components/Body';
 
-export function createBodyGUI(body: Body): GUI {
+export function createBodyController(body: Body): GUI {
   const gui = new GUI();
   gui.add(body, 'name');
 
