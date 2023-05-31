6c127efd6a1598621ff3ed0d83dafc06fae48208
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-31 13:25:49 -0700

    feat: create stub for DataManager class

---
+++ src/World/systems/DataManager.ts
@@ -0,0 +1,10 @@
+/**
+ * class for recording and managing data
+ * collected during the simulation.
+ */
+
+import Body from '../components/Body';
+
+export class DataManager {
+  trackedBody: Body; // Body to track and record data from
+}
