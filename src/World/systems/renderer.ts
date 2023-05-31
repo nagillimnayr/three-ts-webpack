507b617eefe2ca3cde918aa720c4b06157f20d8a
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 20:37:52 -0700

    feat: enable logarithmic depth buffer

--- src/World/systems/renderer.ts
+++ src/World/systems/renderer.ts
@@ -12,6 +12,7 @@ import { WebGLRenderer } from 'three';
 export default function createRenderer() {
   const renderer = new WebGLRenderer({
     antialias: true,
+    logarithmicDepthBuffer: true,
   });
   renderer.domElement.id = 'canvas';
 
