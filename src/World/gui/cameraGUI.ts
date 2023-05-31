b4bbff0bc56d980e83212163303752190e0a08a0
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-31 12:31:50 -0700

    refactor: rename createCameraController to creatCameraGUI

--- src/World/gui/cameraGUI.ts
+++ src/World/gui/cameraGUI.ts
@@ -2,9 +2,7 @@ import GUI from 'lil-gui';
 import { Camera } from 'three';
 import { CameraManager } from '../components/CameraManager';
 
-export default function createCameraController(
-  cameraManager: CameraManager
-): GUI {
+export default function createCameraGUI(cameraManager: CameraManager): GUI {
   const cameraController = new GUI();
   cameraController.title('Camera Controller');
 
