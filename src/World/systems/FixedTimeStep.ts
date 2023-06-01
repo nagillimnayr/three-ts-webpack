5e9ca40e13ca4747715a569903bad2259ae64446
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-01 16:46:30 -0700

    feat: add function factory for fixed update

--- src/World/systems/FixedTimeStep.ts
+++ src/World/systems/FixedTimeStep.ts
@@ -1,3 +1,4 @@
+import { floor } from "mathjs";
 export default class FixedTimeStep {
   private _timeStep: number;
   private _stepsPerSecond: number;
@@ -6,7 +7,7 @@ export default class FixedTimeStep {
 
   constructor(stepsPerSecond: number, updateFn: (timeStep: number) => void) {
     this._stepsPerSecond = stepsPerSecond;
-    this._timeStep = 60 / stepsPerSecond;
+    this._timeStep =  1 / stepsPerSecond;
     this._remainder = 0;
     this._updateFn = updateFn;
   }
@@ -23,7 +24,7 @@ export default class FixedTimeStep {
     const numOfStepsFloat = this._stepsPerSecond * deltaTime + this._remainder;
     // this value will likely be a floating point number, so
     // we must truncate it to an integer
-    const numOfStepsInt = Math.floor(numOfStepsFloat);
+    const numOfStepsInt = floor(numOfStepsFloat);
 
     // Save the truncated part to add it on during the next update
     this._remainder = numOfStepsFloat - numOfStepsInt;
@@ -33,4 +34,38 @@ export default class FixedTimeStep {
       this._updateFn(this._timeStep);
     }
   }
+}
+
+/**
+ * @description Function factory to create update functions which implement
+ * a fixed time-step.
+ * 
+ * @author Ryan Milligan
+ * @date 01/06/2023
+ * @export
+ * @param {number} stepsPerSecond
+ * @param {(timeStep: number) => void} updateFn
+ * @returns {*} 
+ */
+export function makeFixedUpdateFn(stepsPerSecond: number, updateFn: (timeStep: number) => void) {
+  let remainder: number = 0; // Will be captured in closure
+  const timeStep = 1 / stepsPerSecond;
+  
+  const update = (deltaTime: number) => {
+     // determine how many updates we need to do for this frame
+    const numOfStepsFloat = stepsPerSecond * deltaTime + remainder;
+    // this value will likely be a floating point number, so
+    // we must truncate it to an integer
+    const numOfStepsInt = floor(numOfStepsFloat);
+
+    // Save the truncated part to add it on during the next update
+    remainder = numOfStepsFloat - numOfStepsInt;
+
+    // Call the update function the requisite number of times
+    for (let i = 0; i < numOfStepsInt; i++) {
+      updateFn(timeStep);
+    }
+  }
+  
+  return update;
 }
