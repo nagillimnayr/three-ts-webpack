0e587b8955b754b14c78888751264f20e3594dac
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-02 10:55:42 -0700

    feat: modify 'writeToJSON' to accept any generic type

--- src/World/systems/fs/writeToJSON.ts
+++ src/World/systems/fs/writeToJSON.ts
@@ -2,7 +2,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-export async function writeToJSON<T>(dataSet: T[], fileName: string) {
+export async function writeToJSON<T>(dataSet: T, fileName: string) {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
   const pathToNewFile = path.resolve(
